4a1f4193cf0e37e4d9a5b30cbb97571919d88209
Katsunori Kanda <potix2@gmail.com>
2014-08-17 20:01:04 +0900

    add query-result-view

--- src/cljs/isql/core.cljs
+++ src/cljs/isql/core.cljs
@@ -4,12 +4,25 @@
 
 (enable-console-print!)
 
-(def app-state (atom {}))
+(def app-state
+  (atom
+   {:query-result
+    {:columns ["col1" "col2" "col3" "col4" "col5" "col6" "col7" "col8" "col9" "col10"]
+     :rows [
+            [1 2 3 4 5 6 7 8 9 10]
+            [1 2 3 4 5 6 7 8 9 10]
+            [1 2 3 4 5 6 7 8 9 10]
+            [1 2 3 4 5 6 7 8 9 10]
+            [1 2 3 4 5 6 7 8 9 10]
+            [1 2 3 4 5 6 7 8 9 10]
+            [1 2 3 4 5 6 7 8 9 10]
+            ]}}))
 
 (defn active-item [active]
   (if active
     "active"
     ""))
+
 (defn nav-items-view [item owner]
   (reify
     om/IRender
@@ -29,5 +42,23 @@
                                               (om/build-all nav-items-view [{:name "NewQuery"}
                                                                             {:name "Run"}]))))))))
 
+(defn query-result-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+            (let [result (:query-result app)]
+              (dom/div #js {:className "table-responsive"}
+                       (dom/table #js {:className "table table-striped"}
+                                  (dom/thead nil
+                                             (apply dom/tr nil
+                                                    (map #(dom/th nil %) (:columns result))))
+                                  (apply dom/tbody nil
+                                         (map (fn [row]
+                                                (apply dom/tr nil
+                                                       (map #(dom/td nil %) row))) (:rows result)))))))))
+
 (om/root nav-view app-state
          {:target (. js/document (getElementById "navbar"))})
+
+(om/root query-result-view app-state
+         {:target (. js/document (getElementById "query-result"))})
