36e0ae5a8380e5b658376dd6bd18305a28ea60f9
Katsunori Kanda <potix2@gmail.com>
2014-08-19 01:52:10 +0900

    implement mainmenu views

--- src/cljs/isql/appstate.cljs
+++ src/cljs/isql/appstate.cljs
@@ -12,4 +12,8 @@
             [1 2 3 4 5 6 7 8 9 10]
             [1 2 3 4 5 6 7 8 9 10]
             [1 2 3 4 5 6 7 8 9 10]
-            ]}}))
+            ]}
+    :main-menu-items
+    {:new-query {:text "New Query" :command :new-query}
+     :run {:text "Run" :command :run}}
+    }))
--- src/cljs/isql/components/mainmenu.cljs
+++ src/cljs/isql/components/mainmenu.cljs
@@ -1,27 +1,57 @@
 (ns isql.components.mainmenu
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <! alts!]]))
 
-(defn active-item [active]
-  (if active
-    "active"
-    ""))
+(defn run []
+  (.log js/console "run"))
 
-(defn nav-items-view [item owner]
+(defn new-query []
+  (.log js/console "new-query"))
+
+(defn menu-item-component [item owner]
   (reify
-    om/IRender
-    (render [_]
-            (dom/li nil
-                    (dom/a #js {:href "#"} (:name item))))))
+    om/IInitState
+    (init-state [_] {})
+
+    om/IRenderState
+    (render-state [this {:keys [clickchan]}]
+                  (dom/li nil
+                          (dom/a
+                           #js {:href "#"
+                                :onClick (fn [e] (put! clickchan {:command (:command @item)
+                                                                  :owner owner}))}
+                           (:text item))))))
 
 (defn nav-view [app owner]
   (reify
-    om/IRender
-    (render [_]
-            (dom/div #js {:className "navbar navbar-inverse navbar-fixed-top"}
-                     (dom/div #js {:className "navbar-inner"}
-                              (dom/div #js {:className "container"}
-                                       (dom/a #js {:className "brand" :href "#"} "iSQL")
-                                       (apply dom/ul #js {:className "nav"}
-                                              (om/build-all nav-items-view [{:name "NewQuery"}
-                                                                            {:name "Run"}]))))))))
+    om/IInitState
+    (init-state [_]
+                {:clickchan (chan)})
+
+    om/IWillMount
+    (will-mount [_]
+                (let [clickchan (om/get-state owner :clickchan)]
+                  (go (loop []
+                        (let [e (<! clickchan)
+                              command (:command e)
+                              origin-owner (:owner e)]
+                          (when (= command :new-query) (new-query))
+                          (when (= command :run) (run))
+                          (recur))))))
+
+    om/IRenderState
+    (render-state [this {:keys [clickchan]}]
+                  (dom/div #js {:className "navbar navbar-inverse navbar-fixed-top"}
+                           (dom/div #js {:className "navbar-inner"}
+                                    (dom/div #js {:className "container"}
+                                             (dom/a #js {:className "brand" :href "#"} "iSQL")
+                                             (dom/ul #js {:className "nav"}
+                                                     (om/build menu-item-component
+                                                               (get-in app [:main-menu-items :new-query])
+                                                               {:init-state {:clickchan clickchan}})
+                                                     (om/build menu-item-component
+                                                               (get-in app [:main-menu-items :run])
+                                                               {:init-state {:clickchan clickchan}})
+                                                     )))))))
