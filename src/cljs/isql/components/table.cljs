d7a2b07228a634400912825fc935b316f095a285
Katsunori Kanda <potix2@gmail.com>
2014-08-19 23:17:39 +0900

    extract components from query-result-tabale

--- src/cljs/isql/components/table.cljs
+++ src/cljs/isql/components/table.cljs
@@ -2,17 +2,30 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
+(defn table-header [columns owner]
+  (reify
+    om/IRender
+    (render [_]
+            (dom/thead nil
+                       (apply dom/tr nil
+                              (map #(dom/th nil %) columns))))))
+
+(defn table-body [rows owner]
+  (reify
+    om/IRender
+    (render [_]
+            (apply dom/tbody nil
+                   (map (fn [row]
+                          (apply dom/tr nil
+                                 (map #(dom/td nil %) row))) rows)))))
+
 (defn query-result-view [app owner]
   (reify
     om/IRender
     (render [_]
-            (let [result (:query-result app)]
-              (dom/div #js {:className "table-responsive"}
-                       (dom/table #js {:className "table table-striped"}
-                                  (dom/thead nil
-                                             (apply dom/tr nil
-                                                    (map #(dom/th nil %) (:columns result))))
-                                  (apply dom/tbody nil
-                                         (map (fn [row]
-                                                (apply dom/tr nil
-                                                       (map #(dom/td nil %) row))) (:rows result)))))))))
+            (dom/div #js {:className "table-responsive"}
+                     (dom/table #js {:className "table table-striped"}
+                                (om/build table-header
+                                          (get-in app [:query-result :columns]))
+                                (om/build table-body
+                                          (get-in app [:query-result :rows])))))))
