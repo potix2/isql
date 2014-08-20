a8e9e68a72c682a5ddfe5af28ec26c009d9fe338
Katsunori Kanda <potix2@gmail.com>
2014-08-20 09:17:39 +0900

    clear default appstate

--- src/cljs/isql/appstate.cljs
+++ src/cljs/isql/appstate.cljs
@@ -3,16 +3,9 @@
 (def app-state
   (atom
    {:query-result
-    {:columns ["col1" "col2" "col3" "col4" "col5" "col6" "col7" "col8" "col9" "col10"]
-     :rows [
-            [1 2 3 4 5 6 7 8 9 10]
-            [1 2 3 4 5 6 7 8 9 10]
-            [1 2 3 4 5 6 7 8 9 10]
-            [1 2 3 4 5 6 7 8 9 10]
-            [1 2 3 4 5 6 7 8 9 10]
-            [1 2 3 4 5 6 7 8 9 10]
-            [1 2 3 4 5 6 7 8 9 10]
-            ]}
+    {:columns []
+     :rows []}
+
     :main-menu-items
     {:new-query {:text "New Query" :command :new-query}
      :run {:text "Run" :command :run}}
