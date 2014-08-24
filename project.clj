77df6fdd50ad2496aa84cbfab0443f7992d5158b
Katsunori Kanda <potix2@gmail.com>
2014-08-24 13:58:10 +0900

    add the endpoint of api to execute query

--- project.clj
+++ project.clj
@@ -7,9 +7,16 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2311"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.7.1"]]
+                 [om "0.7.1"]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [ring/ring-json "0.3.1"]
+                 [compojure "1.1.6"]]
+
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-ring "0.8.11"]]
+
+  :ring {:handler isql.server.core/app}
 
   :source-paths ["src/clj"]
 
---
+++ src/clj/isql/server/core.clj
@@ -0,0 +1,29 @@
+(ns isql.server.core
+  (:require [compojure.route :as route]
+            [compojure.core :refer [GET POST defroutes]]
+            [compojure.handler :as handler]
+            [ring.util.response :refer [response]]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.json :as middleware]))
+
+(def dummy-result
+  {:columns ["col1" "col2" "col3" "col4" "col5" "col6" "col7" "col8" "col9" "col10"]
+   :rows [
+          [1 2 3 4 5 6 7 8 9 10]
+          [1 2 3 4 5 6 7 8 9 10]
+          [1 2 3 4 5 6 7 8 9 10]
+          [1 2 3 4 5 6 7 8 9 10]
+          [1 2 3 4 5 6 7 8 9 10]
+          [1 2 3 4 5 6 7 8 9 10]
+          [1 2 3 4 5 6 7 8 9 10]
+          ]})
+
+(defroutes app-routes
+  (POST "/queries" {query :query} (response dummy-result))
+  (route/resources "/")
+  (route/not-found "Pagge not found"))
+
+(def app
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response)))
